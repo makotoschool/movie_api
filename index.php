81fa0edff5374970ae8da5de97345bc614b88184
weblabo <sample@v8108.vir.kagoya.net>
2016-01-06 14:27:48 +0900

    リクエストでポスターだけ並ぶようにしてみた

--- index.php
+++ index.php
@@ -53,13 +53,20 @@ require_once('search.php');
 							
 							</form>
 						</div>
+						
                     </header>
-                    <!--
-                    <section>
-                        <h2>ドラマ検索</h2>
-                        <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit. Aliquam sodales urna non odio egestas tempor. Nunc vel vehicula ante. Etiam bibendum iaculis libero, eget molestie nisl pharetra in. In semper consequat est, eu porta velit mollis nec. Curabitur posuere enim eget turpis feugiat tempor. Etiam ullamcorper lorem dapibus velit suscipit ultrices. Proin in est sed erat facilisis pharetra.</p>
-                    </section>
                     
+                    <section>
+                    	<?php foreach($obj['results'] as $movie):?>
+                    		<p><img src="http://image.tmdb.org/t/p/w500/<?php echo $movie['poster_path'];?>"></p>
+							
+							
+							
+						<?php endforeach;?>
+                    
+                    
+                    </section>
+                    <!--
                     <section>
                         <h2>article section h2</h2>
                         <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit. Aliquam sodales urna non odio egestas tempor. Nunc vel vehicula ante. Etiam bibendum iaculis libero, eget molestie nisl pharetra in. In semper consequat est, eu porta velit mollis nec. Curabitur posuere enim eget turpis feugiat tempor. Etiam ullamcorper lorem dapibus velit suscipit ultrices. Proin in est sed erat facilisis pharetra.</p>
@@ -74,7 +81,6 @@ require_once('search.php');
 
                 <aside>
                     <h3>aside</h3>
-                    <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit. Aliquam sodales urna non odio egestas tempor. Nunc vel vehicula ante. Etiam bibendum iaculis libero, eget molestie nisl pharetra in. In semper consequat est, eu porta velit mollis nec. Curabitur posuere enim eget turpis feugiat tempor. Etiam ullamcorper lorem dapibus velit suscipit ultrices.</p>
                 </aside>
 
             </div> <!-- #main -->
--- search.php
+++ search.php
@@ -1,4 +1,18 @@
 <?php
-$json = file_get_contents("https://api.themoviedb.org/3/search/tv?api_key=002e50c7a2505b32c630b7bd2f07b82b&query=fringe");
+// ファイルからJSONを読み込み
+$json = file_get_contents("https://api.themoviedb.org/3/search/movie?api_key=002e50c7a2505b32c630b7bd2f07b82b&query=a%");
 
+// 文字化けするかもしれないのでUTF-8に変換
+$json = mb_convert_encoding($json, 'UTF8', 'ASCII,JIS,UTF-8,EUC-JP,SJIS-WIN');
+// オブジェクト毎にパース
+// trueを付けると連想配列として分解して格納してくれます。
+$obj = json_decode($json, true);
+ 
+// パースに失敗した時は処理終了
+if ($obj === NULL) {
+return;
+}
 
+echo '<pre>';
+print_r($obj);
+echo '</pre>';
